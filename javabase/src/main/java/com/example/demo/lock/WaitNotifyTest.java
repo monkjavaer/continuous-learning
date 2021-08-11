226e0b0d50a70996baad45de10b325989a0a863e
monkjavaer <1269106963@qq.com>
2021-08-11 23:56:38 +0800

    condition test

--- javabase/src/main/java/com/example/demo/lock/WaitNotifyTest.java
+++ javabase/src/main/java/com/example/demo/lock/WaitNotifyTest.java
@@ -21,8 +21,8 @@ public class WaitNotifyTest {
         ThreadNotify threadNotify = test.new ThreadNotify("消费者：");
         threadNotify.start();
 
-        for (int i = 0 ; i < 15; i++ ) {
-            ThreadWait threadWait1 = test.new ThreadWait("生产者"+ i +"：");
+        for (int i = 0; i < 15; i++) {
+            ThreadWait threadWait1 = test.new ThreadWait("生产者" + i + "：");
             threadWait1.start();
         }
 
@@ -30,16 +30,16 @@ public class WaitNotifyTest {
 
     class ThreadWait extends Thread {
         //生产者
-        public ThreadWait(String name){
+        public ThreadWait(String name) {
             super(name);
         }
 
         @Override
         public void run() {
             synchronized (queue) {
-                if (queue.size() > max) {
-                    System.out.println("线程" + this.getName() + "消费队列满，开始等待队列空闲");
+                while (queue.size() >= max) {
                     try {
+                        System.out.println("线程" + this.getName() + "消费队列满，开始等待队列空闲");
                         long startTime = System.currentTimeMillis();
                         //挂起当前线程，释放通过同步块获取的对象锁。
                         queue.wait();
@@ -48,47 +48,36 @@ public class WaitNotifyTest {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }else {
-                    //队列没有满，生产消息。
-                    try {
-                        String msg = UUID.randomUUID().toString();
-                        queue.put(msg);
-                        System.out.println("线程" + this.getName() + "生产一条数据：" + msg);
-
-                        System.out.println("线程" + this.getName() + "发完消息，开始准备通知");
-                        queue.notifyAll();
-                        System.out.println("线程" + this.getName() + "通知结束");
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                 }
+                //队列没有满，生产消息。
+                try {
+                    String msg = UUID.randomUUID().toString();
+                    queue.put(msg);
+                    System.out.println("线程" + this.getName() + "生产一条数据：" + msg);
+
+                    System.out.println("线程" + this.getName() + "发完消息，开始准备通知");
+                    queue.notifyAll();
+                    System.out.println("线程" + this.getName() + "通知结束");
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
     }
 
     class ThreadNotify extends Thread {
         //消费者
-        public ThreadNotify(String name){
+        public ThreadNotify(String name) {
             super(name);
         }
+
         @Override
         public void run() {
             while (true) {
                 synchronized (queue) {
-                    if (queue.size() > 0) {
-                        try {
-                            // 给等待线程等待时间
-                            sleep(3000);
-                            System.out.println("线程" + this.getName() + "消费一条数据：" + queue.take());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        System.out.println("线程" + this.getName() + "开始准备通知");
-                        queue.notifyAll();
-                        System.out.println("线程" + this.getName() + "通知结束");
-                    }else {
+                    while (queue.size() == 0) {
                         //消费完了，开始等待
                         try {
                             System.out.println("线程" + this.getName() + "消费完了，开始等待");
@@ -100,6 +89,19 @@ public class WaitNotifyTest {
                             e.printStackTrace();
                         }
                     }
+
+
+                    try {
+                        // 给等待线程等待时间
+                        sleep(3000);
+                        System.out.println("线程" + this.getName() + "消费一条数据：" + queue.take());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("线程" + this.getName() + "开始准备通知");
+                    queue.notifyAll();
+                    System.out.println("线程" + this.getName() + "通知结束");
 
                 }
             }
